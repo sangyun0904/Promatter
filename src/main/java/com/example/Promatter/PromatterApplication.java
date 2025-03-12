0d597c782a9bdbe6370b94448250c19c50e24d37
sangyoon kim <sangyun0904@gmail.com>
2025-03-12 19:18:30 +0900

    Initiate Project

---
+++ src/main/java/com/example/Promatter/PromatterApplication.java
@@ -0,0 +1,13 @@
+package com.example.Promatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromatterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Promatter/PromatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Promatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
